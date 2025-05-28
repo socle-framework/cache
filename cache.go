9ff365c4bcc26167c0acb7ddfc63623defa90de2
AKAKPO K. M. Daniel <it.danielkeho@gmail.com>
2025-05-28 11:51:00 +0000

    initial commit

---
+++ cache.go
@@ -0,0 +1,10 @@
+package cache
+
+type Cache interface {
+	Has(string) (bool, error)
+	Get(string) (interface{}, error)
+	Set(string, interface{}, ...int) error
+	Forget(string) error
+	EmptyByMatch(string) error
+	Empty() error
+}
